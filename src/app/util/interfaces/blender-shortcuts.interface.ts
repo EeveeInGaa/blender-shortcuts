d1a0d9970aab28f8c129950f7a3a957e60979cb8
inga <inga@qupaya.com>
2024-04-08 12:12:20 +0200

    refactor: refactor interface for shortcuts

--- src/app/util/interfaces/blender-shortcuts.interface.ts
+++ src/app/util/interfaces/blender-shortcuts.interface.ts
@@ -2,9 +2,14 @@ import { BlenderArea } from '../enums/blender-area.enum';
 
 export interface BlenderShortcuts {
   area: BlenderArea[];
-  title?: string; //optional title to find faster
+  //title to find faster
+  title: string;
+  //array to add plus in between
   shortcut: string[];
-  description: string[]; //array in case its more than one
-  information?: string[]; //optional hints; array in case its more than one
-  tags?: string[]; //tags to find faster
+  //array in case it's more than one
+  description: string[];
+  //optional hints; array in case it's more than one
+  information?: string[];
+  //tags to find topic faster
+  tags: string[];
 }
